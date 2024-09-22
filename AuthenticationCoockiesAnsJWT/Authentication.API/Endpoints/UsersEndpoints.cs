b2bc1ac47ed58aa3de57bd1ba7490b3d847d4bdf
Artur <guart05@mail.ru>
2024-09-23 00:31:58 +0300

    passwordhasher done

---
+++ AuthenticationCoockiesAnsJWT/Authentication.API/Endpoints/UsersEndpoints.cs
@@ -0,0 +1,28 @@
+﻿using Authentication.API.Contracts.Users;
+using Authentication.Application.Services;
+
+namespace Authentication.API.Endpoints
+{
+    public static class UsersEndpoints
+    {
+        public static IEndpointRouteBuilder MapUsersEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("register", Register);
+            app.MapPost("login", Login);
+
+            return app;
+        }
+
+        public static async Task<IResult> Register(RegisterUserRequest request, UsersService usersService)
+        {
+            await usersService.Register(request.UserName, request.Email, request.Password);
+
+            return Results.Ok();
+        }
+
+        public static async Task<IResult> Login()
+        {
+            return Results.Ok();
+        }
+    }
+}
---
+++ AuthenticationCoockiesAnsJWT/Authentication.Application/Services/UsersService.cs
@@ -0,0 +1,30 @@
+﻿using Authentication.Application.Interfaces.Auth;
+using Authentication.Application.Interfaces.Repositories;
+using Authentication.Core.Models;
+
+namespace Authentication.Application.Services
+{
+    public class UsersService
+    {
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IUsersRepository _usersRepository;
+        public UsersService(IUsersRepository usersRepository, IPasswordHasher passwordHasher)
+        {
+            _passwordHasher = passwordHasher;
+        }
+        public async Task Register(string userName, string email, string password)
+        {
+            var hashedPassword = _passwordHasher.Generate(password);
+
+            var user = User.Create(Guid.NewGuid(), userName, email, hashedPassword);
+
+            await _usersRepository.Add(user);
+        }
+
+        public async Task<string> Login(string email, string password)
+        {
+            // 10.16
+            return "";
+        }
+    }
+}
